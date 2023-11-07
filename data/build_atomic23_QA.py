a2c4e462d7e94c24d91f1ca2b379c290ae91bc67
maryamfz <6569288+maryamfz@users.noreply.github.com>
2023-11-07 17:34:45 -0500

    Update build_atomic23_QA.py
    Updated copyright

--- data/build_atomic23_QA.py
+++ data/build_atomic23_QA.py
@@ -1,3 +1,6 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# This software may be used and distributed according to the terms of the GNU General Public License version 3.
+
 import json
 import csv
 import os
